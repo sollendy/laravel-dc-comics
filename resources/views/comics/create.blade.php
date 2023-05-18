5600bd72873fffe625cf3e6bcb3fd216bfe5843d
sollendy <edlannino@tutanota.com>
2023-05-18 17:55:04 +0200

    feat: aggiunta funzione old() anche nel create

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -7,7 +7,7 @@
     
       <div class="mb-3">
         <label for="title">Titolo</label>
-        <input class="form-control @error('title') is-invalid @enderror" type="text" name="title" id="title">
+        <input class="form-control @error('title') is-invalid @enderror" type="text" name="title" id="title" value="{{old('title')}}">
           @error('title')
             <div class="invalid-feedback">
               {{$message}}
@@ -17,7 +17,7 @@
     
       <div class="mb-3">
         <label for="description">Descrizione</label>
-        <textarea class="form-control @error('description') is-invalid @enderror" name="description" id="description"></textarea>
+        <textarea class="form-control @error('description') is-invalid @enderror" name="description" id="description" value="{{old('description')}}"></textarea>
           @error('description')
             <div class="invalid-feedback">
               {{$message}}
@@ -27,7 +27,7 @@
     
       <div class="mb-3">
         <label for="thumb">Fornisci immagine</label>
-        <input class="form-control @error('thumb') is-invalid @enderror" type="text" name="thumb" id="thumb">
+        <input class="form-control @error('thumb') is-invalid @enderror" type="text" name="thumb" id="thumb" value="{{old('thumb')}}">
         @error('thumb')
             <div class="invalid-feedback">
               {{$message}}
@@ -37,7 +37,7 @@
     
       <div class="mb-3">
         <label for="price">Prezzo</label>
-        <input class="form-control @error('price') is-invalid @enderror" type="number" name="price" id="price">
+        <input class="form-control @error('price') is-invalid @enderror" type="number" name="price" id="price" value="{{old('price')}}">
         @error('price')
             <div class="invalid-feedback">
               {{$message}}
@@ -47,7 +47,7 @@
     
       <div class="mb-3">
         <label for="series">Serie</label>
-        <input class="form-control @error('series') is-invalid @enderror" type="text" name="series" id="series">
+        <input class="form-control @error('series') is-invalid @enderror" type="text" name="series" id="series" value="{{old('series')}}">
         @error('series')
             <div class="invalid-feedback">
               {{$message}}
@@ -57,7 +57,7 @@
     
       <div class="mb-3">
         <label for="sale_date">Data di lancio</label>
-        <input class="form-control @error('sale_date') is-invalid @enderror" type="date" name="sale_date" id="sale_date">
+        <input class="form-control @error('sale_date') is-invalid @enderror" type="date" name="sale_date" id="sale_date" value="{{old('sale_date')}}">
         @error('sale_date')
             <div class="invalid-feedback">
               {{$message}}
@@ -67,7 +67,7 @@
     
       <div class="mb-3">
         <label for="type">Tipo</label>
-        <input class="form-control @error('type') is-invalid @enderror" type="text" name="type" id="type">
+        <input class="form-control @error('type') is-invalid @enderror" type="text" name="type" id="type" value="{{old('type')}}">
         @error('type')
             <div class="invalid-feedback">
               {{$message}}
